4179b4b0b5297c554c623329e64b1824b2730ea9
Tod Wulff <tod.wulff@gmail.com>
2025-05-09 08:16:00 -0400

    testing commit push script

--- conky_scripts/git_push.sh
+++ conky_scripts/git_push.sh
@@ -2,20 +2,24 @@
 
 # Check if any parameters were provided directly
 if [ $# -eq 0 ]; then
+    echo ""
     echo "Commit message must be provided - aborting..."
+    echo ""
     exit 1
 else
     # Combine all parameters into a single commit message
     params="$@"
-    
+    echo ""
     echo "Pushing Conky widgets and scripts to github repo."
     echo "Commit Message: $params"
-    
+    echo ""
     cd ~/.conky/MHz_conky_widgets/
     git fetch && git status
     git add .
     git commit -m "$params"
     git push
+    echo ""
     echo "Pushed Conky widgets and scripts to github repo"
     echo "Commit Message: $params"
+    echo ""
 fi
